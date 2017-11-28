06e99d1efafe825480b07b2a9b1de5811ad37114
William Puerta <Williampuerta1097@gmail.com>
2017-11-28 02:21:48 -0500

    Fixing EnterSubjects.java code errors

--- SIGNOTEST/src/Interfaces/EnterSubjects.java
+++ SIGNOTEST/src/Interfaces/EnterSubjects.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Modulos;
+package Interfaces;
 
-import extras.Connection;
+import Resources.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
