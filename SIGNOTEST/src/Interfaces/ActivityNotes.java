6ce53f01617fa86cfe1a48dfff056385c0fbc575
JHONATAN RESTREPO <jhonatan1210@gmail.com>
2017-12-01 19:40:45 -0500

    Change of package

--- SIGNOTEST/src/Interfaces/ActivityNotes.java
+++ SIGNOTEST/src/Interfaces/ActivityNotes.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package signo;
+package Interfaces;
 
+import Resources.Connection;
 import java.awt.event.ItemEvent;
 import java.sql.ResultSet;
 import javax.swing.DefaultComboBoxModel;
