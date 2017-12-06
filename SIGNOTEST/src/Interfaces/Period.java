09a10fdbc27c683b63ab23db46a5f49669da7f55
JHONATAN RESTREPO <jhonatan1210@gmail.com>
2017-12-06 18:13:51 -0500

    Library add

--- SIGNOTEST/src/Interfaces/Period.java
+++ SIGNOTEST/src/Interfaces/Period.java
@@ -1,10 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package Interfaces;
 
 
+import Resources.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
