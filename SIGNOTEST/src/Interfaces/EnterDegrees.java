846f60d870e6fb0999f8de132471cc4e84f56113
JHONATAN RESTREPO <jhonatan1210@gmail.com>
2017-12-06 08:00:03 -0500

    Modify code EnterDegrees

--- SIGNOTEST/src/Interfaces/EnterDegrees.java
+++ SIGNOTEST/src/Interfaces/EnterDegrees.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package signo;
+package Interfaces;
+
+import Resources.Connection;
 
 import java.sql.ResultSet;
 import javax.swing.JMenuItem;
