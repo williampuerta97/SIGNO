5e9c733e2048127f8ea44d7b3b0ccb3db81332f5
JHONATAN RESTREPO <jhonatan1210@gmail.com>
2017-12-11 07:40:39 -0500

    Modify code EnterSubjects

--- SIGNOTEST/src/Interfaces/EnterSubjects.java
+++ SIGNOTEST/src/Interfaces/EnterSubjects.java
@@ -3,14 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package signo;
+package Interfaces;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
-import signo.Connection;
+import Resources.Connection;
 
 /**
  *
