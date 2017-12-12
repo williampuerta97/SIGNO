d37b42b99133e78c91d8a21606c0cadaf588c060
JHONATAN RESTREPO <jhonatan1210@gmail.com>
2017-12-12 11:10:40 -0500

    Modifiying Code nationality

--- SIGNOTEST/src/Interfaces/Nationality.java
+++ SIGNOTEST/src/Interfaces/Nationality.java
@@ -29,6 +29,7 @@ public class Nationality extends javax.swing.JInternalFrame {
 
         jDesktopPaneFondo = new javax.swing.JDesktopPane();
         jLabelTitulo = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         jDesktopPaneFondo.setBackground(new java.awt.Color(0, 0, 255));
 
@@ -37,20 +38,37 @@ public class Nationality extends javax.swing.JInternalFrame {
         jLabelTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabelTitulo.setText("CREACION DE NACIONALIDAD");
 
+        jButton1.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/Mapais.png"))); // NOI18N
+        jButton1.setText("PAIS");
+        jButton1.setBorder(null);
+        jButton1.setBorderPainted(false);
+        jButton1.setContentAreaFilled(false);
+        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jButton1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+
         jDesktopPaneFondo.setLayer(jLabelTitulo, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jDesktopPaneFondo.setLayer(jButton1, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         javax.swing.GroupLayout jDesktopPaneFondoLayout = new javax.swing.GroupLayout(jDesktopPaneFondo);
         jDesktopPaneFondo.setLayout(jDesktopPaneFondoLayout);
         jDesktopPaneFondoLayout.setHorizontalGroup(
             jDesktopPaneFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabelTitulo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabelTitulo, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1240, Short.MAX_VALUE)
+            .addGroup(jDesktopPaneFondoLayout.createSequentialGroup()
+                .addGap(117, 117, 117)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 189, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jDesktopPaneFondoLayout.setVerticalGroup(
             jDesktopPaneFondoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jDesktopPaneFondoLayout.createSequentialGroup()
                 .addGap(27, 27, 27)
                 .addComponent(jLabelTitulo)
-                .addContainerGap(628, Short.MAX_VALUE))
+                .addGap(106, 106, 106)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(327, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -69,6 +87,7 @@ public class Nationality extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JDesktopPane jDesktopPaneFondo;
     private javax.swing.JLabel jLabelTitulo;
     // End of variables declaration//GEN-END:variables
